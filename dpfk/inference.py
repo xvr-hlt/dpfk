13ab03270e542e1e431da294f3b89eb7da03929a
xvr.hlt <holt.xavier@gmail.com>
2020-03-16 10:15:40 +1100

    added inf code

---
+++ dpfk/inference.py
@@ -0,0 +1,28 @@
+import torch
+from torchvision import transforms
+
+from dpfk import util
+from dpfk.data import util as data_util
+from dpfk.nn import model
+
+
+class InferenceEngine:
+
+    def __init__(self, config, weights, device='cuda'):
+        config = util.read_config(config)
+        self.model, normalize = model.get_model_normalize_from_config(
+            config, pretrained=False)
+        util.load_weights(self.model, weights)
+        self.model = self.model.to(device)
+        self.preprocess = transforms.Compose([transforms.ToTensor(), normalize])
+        self.device = device
+
+    @torch.no_grad()
+    def __call__(self, vid_path):
+        preds = []
+        frames = data_util.grab_frames(vid_path)
+        for frame in frames:
+            frame = self.preprocess(frame).to(self.device)
+            frame = frame[None]
+            preds.append(self.model(frame).sigmoid())
+        return torch.cat(preds).mean()
